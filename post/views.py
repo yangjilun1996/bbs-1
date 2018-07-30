95002794fe8a9d358c31f183bf12365e0af89e61
Seamile <lanhuermao@gmail.com>
2018-07-30 13:36:13 +0800

    完成了帖子列表及分页功能

--- post/views.py
+++ post/views.py
@@ -1,10 +1,22 @@
+from math import ceil
+
 from django.shortcuts import render, redirect
 
 from post.models import Post
 
 
 def post_list(request):
-    return render(request, 'post_list.html', {})
+    page = int(request.GET.get('page', 1))  # 当前页码
+    total = Post.objects.count()            # 帖子总数
+    per_page = 10                           # 每页帖子数
+    pages = ceil(total / per_page)          # 总页数
+
+    start = (page - 1) * per_page
+    end = start + per_page
+
+    posts = Post.objects.all().order_by('-id')[start:end]  # 惰性加载 懒加载
+    return render(request, 'post_list.html',
+                  {'posts': posts, 'pages': range(pages)})
 
 
 def create_post(request):
