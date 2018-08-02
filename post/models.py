de6c41df84853d95d9d4bb0ed972f650e7af6b62
Seamile <lanhuermao@gmail.com>
2018-08-02 14:53:25 +0800

    添加了 Tag 功能

--- post/models.py
+++ post/models.py
@@ -18,6 +18,25 @@ class Post(models.Model):
     def comments(self):
         return Comment.objects.filter(post_id=self.id).order_by('-id')
 
+    def tags(self):
+        relations = PostTagRelation.objects.filter(post_id=self.id).only('tag_id')
+        tag_id_list = [r.tag_id for r in relations]
+        return Tag.objects.filter(id__in=tag_id_list)
+
+    def update_tags(self, tag_names):
+        Tag.ensure_tags(tag_names)
+
+        update_names = set(tag_names)
+        exist_names = {t.name for t in self.tags()}
+
+        # 筛选出需要新创建的关系
+        need_create_names = update_names - exist_names
+        PostTagRelation.add_post_tags(self.id, need_create_names)
+
+        # 筛选出需要删除的关系
+        need_delete_names = exist_names - update_names
+        PostTagRelation.del_post_tags(self.id, need_delete_names)
+
 
 class Comment(models.Model):
     uid = models.IntegerField()
@@ -36,3 +55,48 @@ class Comment(models.Model):
         if not hasattr(self, '_post'):
             self._post = Post.objects.get(id=self.post_id)
         return self._post
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=16, unique=True)
+
+    @classmethod
+    def ensure_tags(cls, tag_names):
+        '''确保传入的 name 存在'''
+        tags = cls.objects.filter(name__in=tag_names).only('name')
+        exist_names = {t.name for t in tags}
+        new_names = set(tag_names) - exist_names
+        need_create = [cls(name=name) for name in new_names]
+        cls.objects.bulk_create(need_create)
+
+    def posts(self):
+        relations = PostTagRelation.objects.filter(tag_id=self.id).only('post_id')
+        post_id_list = [r.post_id for r in relations]
+        return Post.objects.filter(id__in=post_id_list)
+
+
+class PostTagRelation(models.Model):
+    '''
+    Post 和 Tag 的关系表
+        Python编码风格       python
+        Python编码风格       codestyle
+        Django部署          python
+        Django部署          django
+        Django部署          linux
+        Nginx开发           nginx
+        Nginx开发           django
+        Nginx开发           linux
+    '''
+    post_id = models.IntegerField()
+    tag_id = models.IntegerField()
+
+    @classmethod
+    def add_post_tags(cls, post_id, tag_names):
+        tag_id_list = [t.id for t in Tag.objects.filter(name__in=tag_names)]
+        relations = [cls(post_id=post_id, tag_id=tid) for tid in tag_id_list]
+        cls.objects.bulk_create(relations)
+
+    @classmethod
+    def del_post_tags(cls, post_id, tag_names):
+        tag_id_list = [t.id for t in Tag.objects.filter(name__in=tag_names)]
+        cls.objects.filter(post_id=post_id, tag_id__in=tag_id_list).delete()
--- post/views.py
+++ post/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 
 from post.models import Post
 from post.models import Comment
+from post.models import Tag
 from post.helper import page_cache
 from post.helper import read_count
 from post.helper import get_top_n
@@ -44,11 +45,18 @@ def edit_post(request):
         post.title = request.POST.get('title')
         post.content = request.POST.get('content')
         post.save()
+
+        str_tags = request.POST.get('tags')
+        tag_names = [s.strip()
+                     for s in str_tags.title().replace('，', ',').split(',')
+                     if s.strip()]
+        post.update_tags(tag_names)
         return redirect('/post/read/?post_id=%s' % post.id)
     else:
         post_id = request.GET.get('post_id')
         post = Post.objects.get(id=post_id)
-        return render(request, 'edit_post.html', {'post': post})
+        str_tags = ', '.join(t.name for t in post.tags())
+        return render(request, 'edit_post.html', {'post': post, 'tags': str_tags})
 
 
 @read_count
@@ -85,3 +93,9 @@ def comment(request):
     content = request.POST.get('content')
     Comment.objects.create(uid=uid, post_id=post_id, content=content)
     return redirect('/post/read/?post_id=%s' % post_id)
+
+
+def tag_filter(request):
+    tag_id = int(request.GET.get('tag_id'))
+    tag = Tag.objects.get(id=tag_id)
+    return render(request, 'tag_filter.html', {'posts': tag.posts()})
